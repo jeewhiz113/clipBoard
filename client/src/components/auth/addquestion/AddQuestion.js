c4422117ddfe13177e7da7289ccacfa9f774dd84
Jee Zhou <jeezhou@Jees-iMac.local>
2020-09-04 21:15:26 -0500

    work on adding pictures

--- client/src/components/auth/addquestion/AddQuestion.js
+++ client/src/components/auth/addquestion/AddQuestion.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import {
   Button,
@@ -13,18 +14,19 @@ import {
   Alert
 } from 'reactstrap';
 
-import { connect } from 'react-redux';
+/* import { connect } from 'react-redux'; */
 
 
 const AddQuestion = (props) => {
-  
+  const {register, handleSubmit} = useForm();
   const [modal, setModal] = useState(false);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [msg, setMsg] = useState(null);
+  const [files, setFile] = useState([]);
+  //const [password, setPassword] = useState('');
+  //const [msg, setMsg] = useState(null);
   const toggle = ()=>{
     setModal(!modal);
   }
+  
   /*
   useEffect(()=>{
     const {error} = props;
@@ -46,29 +48,23 @@ const AddQuestion = (props) => {
   
   const onSubmit = e =>{
     e.preventDefault();
-    const authUser = {
+    /* const authUser = {
       email,
       password
-    };
+    }; */
     //props.login(authUser);
   }
     return(
       <div>
         <NavLink onClick={toggle} href="#">Add Question</NavLink>
         <Modal isOpen = {modal} toggle = {toggle}>
-          <ModalHeader toggle = {toggle}>Add a Quetion</ModalHeader>
-            <ModalBody>
-              {msg? <Alert color='danger'>{msg}</Alert> : null}
-              <Form onSubmit = {onSubmit}>
-                <FormGroup>
-                  <Label for='email'>Email</Label>
-                  <Input type = 'email' name="email" id = "email" placeholder = "Email" className="mb-3" />
-                  <Label for='password'>Password</Label>
-                  <Input type = 'password' name="password" id = "password" placeholder = "Password" className="mb-3" />
-                  <Button color='dark' style={{marginTop: '2rem'}} block> Login</Button>
-                </FormGroup>
-              </Form>
-            </ModalBody>
+        < form >
+        <div>
+          <h2>Upload images</h2>
+        </div>
+        <h3>Images</h3>
+        <input type="file" multiple  />
+      </form>
           
         </Modal>
       </div>
@@ -81,6 +77,21 @@ const mapStateToProps = (state) =>({
   error: state.error
 }) 
 
-
+/*
+onChange={this.fileSelectedHandler}
+<ModalHeader toggle = {toggle}>Add a Quetion</ModalHeader>
+            <ModalBody>
+              {msg? <Alert color='danger'>{msg}</Alert> : null}
+              <Form onSubmit = {onSubmit}>
+                <FormGroup>
+                  <Label for='email'>Email</Label>
+                  <Input type = 'email' name="email" id = "email" placeholder = "Email" className="mb-3" />
+                  <Label for='password'>Password</Label>
+                  <Input type = 'password' name="password" id = "password" placeholder = "Password" className="mb-3" />
+                  <Button color='dark' style={{marginTop: '2rem'}} block> Login</Button>
+                </FormGroup>
+              </Form>
+            </ModalBody>
+*/
 export default AddQuestion;
 
